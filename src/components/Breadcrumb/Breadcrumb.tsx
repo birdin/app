87a7b6d263824dcbe9b5afc9a2254a521dd2f4d2
iCharlieAraiza <eliptikman1234@gmail.com>
2023-05-06 05:56:46 -0600

    Add breck word function and update breadcrumbs

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import { FcFolder } from "react-icons/fc";
 
 type Props = {
     name: string | undefined;
     id: string | undefined; 
+    page: string | undefined;
 }
 
 
-const Breadcrumb = ({name, id}: Props) => {
+const Breadcrumb = ({name, id, page = 'Homepage'}: Props) => {
   return (
     <div className='wide-container breadcrumb-container {
         '>
-        <Link to={`/project/${id}`}>{ name }</Link>
+        <Link to={`/project/${id}`}>
+            <FcFolder/> { name }
+        </Link>
         <span>/</span>
-        <span>Homepage</span>
+        <span> {page}</span>
     </div>
   )
 }
